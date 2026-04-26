5b1de2badcfb886e70fa195e1a17caef74b798f8
saadtalal1 <saadtalal0112@gmail.com>
2026-04-26 12:26:49 +0400

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,7 @@
+a=[]
+b=["Saad","Max","Alex",1,2,3,4,5]
+print(b*3)
+list.reverse(b)
+print(b)
+print(b[::-1])
+print(a)
---
+++ Activity 2.py
@@ -0,0 +1,6 @@
+a=["Saad","ABA","KKK","Max","Abdul"]
+b=[]
+for i in a:
+    if i[0]==i[-1]:
+        print(b.append(i))
+        
